39ae771a7e821ab5d7c88474cfa618c15099cd3a
Sangram Jagadale <58004845+Sangram3@users.noreply.github.com>
2021-10-25 23:24:47 +0530

    Create CheckGrades.sql

---
+++ CheckGrades.sql
@@ -0,0 +1,26 @@
+
+
+CREATE OR REPLACE FUNCTION CheckGrades(
+	IN student_id_ integer,
+	IN year_ integer,
+	IN semester_ integer
+)
+
+RETURNS TABLE(
+	course_id text,
+	grade integer
+)
+LANGUAGE plpgsql
+AS
+$$
+	BEGIN
+		RETURN QUERY
+		SELECT g.course_id ,g.grade FROM Grades AS g
+		WHERE g.year = year 
+		and g.semester = semester_
+		and g.student_id = student_id_;
+	
+	END;
+$$;
+
+
