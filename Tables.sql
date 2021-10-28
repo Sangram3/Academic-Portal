599e829d6e6c9f2fa69bdaf462413a5b3f149290
Sangram Jagadale <58004845+Sangram3@users.noreply.github.com>
2021-10-28 20:09:12 +0530

    Update Tables.sql

--- Tables.sql
+++ Tables.sql
@@ -1,15 +1,91 @@
-DROP TABLE Grades;
---Grades (course_id , student_id , semester , year , grade )
+DROP TABLE IF EXISTS Grades CASCADE;
+DROP TABLE IF EXISTS CourseCatalogue CASCADE;
+DROP TABLE IF EXISTS StudentRegistrationTable CASCADE;
+DROP TABLE IF EXISTS TicketTable CASCADE;
+DROP TABLE IF EXISTS Students CASCADE;
+DROP TABLE IF EXISTS Instructors CASCADE;
+DROP TABLE IF EXISTS FacultyAdvisors CASCADE;
+DROP TABLE IF EXISTS DeanFA CASCADE;
+DROP TABLE IF EXISTS TimeTableSlots CASCADE;
+DROP TABLE IF EXISTS CourseOfferings CASCADE;
+
+CREATE TABLE Students(
+	student_id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	student_name text NOT NULL,
+	year_joined integer NOT NULL
+);
+
+CREATE TABLE Instructors(
+	instructor_id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	instructor_name text NOT NULL
+);
+
+CREATE TABLE FacultyAdvisors(
+	fa_id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	fa_name text NOT NULL
+);
+
+CREATE TABLE DeanFA(
+	dean_id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	dean_name text NOT NULL
+);
+
+CREATE TABLE TimeTableSlots(
+	slot_id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	week_day text NOT NULL,
+	start_time time NOT NULL,
+	end_time time NOT NULL
+);
+
+CREATE TABLE CourseCatalogue(
+	course_id text PRIMARY KEY,
+	course_name text NOT NULL,
+	L integer,
+	T integer,
+	P integer,
+	S integer,
+	C real NOT NULL,
+	prerequisite text -- course_id	
+);
+
+CREATE TABLE CourseOfferings(
+	course_id text NOT NULL,
+	instructor_id integer NOT NULL,
+	year integer NOT NULL,
+	semester integer NOT NULL,
+	section integer NOT NULL,
+	slot_id integer NOT NULL,
+	cgpa_cutoff real,
+	branch text,
+	
+	FOREIGN KEY(course_id) 
+	REFERENCES CourseCatalogue(course_id)
+	ON DELETE CASCADE,
+	
+	FOREIGN KEY(slot_id) 
+	REFERENCES TimeTableSlots(slot_id)
+	ON DELETE CASCADE,
+	
+	FOREIGN KEY(instructor_id) 
+	REFERENCES Instructors(instructor_id)
+	ON DELETE CASCADE
+);
 
 CREATE TABLE Grades(
-	course_id text,
-	student_id integer,
-	year integer,
-	semester integer,
-	grade integer -- in the format 1,2,...,10 and not A+ A-
-)
-
-DROP TABLE StudentRegistrationTable
+	course_id text NOT NULL,
+	student_id integer NOT NULL,
+	year integer NOT NULL,
+	semester integer NOT NULL,
+	grade integer NOT NULL,
+	
+	FOREIGN KEY(course_id) 
+	REFERENCES CourseCatalogue(course_id)
+	ON DELETE CASCADE,
+	
+	FOREIGN KEY(student_id) 
+	REFERENCES Students(student_id)
+	ON DELETE CASCADE
+);
 
 CREATE TABLE StudentRegistrationTable(
 	student_id integer NOT NULL,
@@ -17,35 +93,21 @@ CREATE TABLE StudentRegistrationTable(
 	semester integer NOT NULL,
 	year integer NOT NULL,
 	section integer NOT NULL,
-	slot text NOT NULL
-);
-
-INSERT INTO StudentRegistrationTable VALUES(
-	12,'CS201',1,2021,1,'s1'
-);
-INSERT INTO StudentRegistrationTable VALUES(
-	13,'CS201',1,2021,1,'s4'
-);
-INSERT INTO StudentRegistrationTable VALUES(
-	12,'CS204',1,2021,1,'s2'
-);
-INSERT INTO StudentRegistrationTable VALUES(
-	12,'CS208',1,2020,1,'s3'
-);
-
-
-CREATE TABLE CourseCatalogue(
-	course_id text,
-	course_name text,
-	L integer,
-	T integer,
-	P integer,
-	S integer,
-	C real,
-	prerequisite text -- course_id
+	slot_id integer NOT NULL,
+	
+	FOREIGN KEY(student_id) 
+	REFERENCES Students(student_id)
+	ON DELETE CASCADE,
+	
+	FOREIGN KEY(course_id) 
+	REFERENCES CourseCatalogue(course_id)
+	ON DELETE CASCADE,
+	
+	FOREIGN KEY(slot_id) 
+	REFERENCES TimeTableSlots(slot_id)
+	ON DELETE CASCADE
 	
 );
-
 
 CREATE TABLE TicketTable(
 	ticket_id integer PRIMARY KEY,
@@ -53,6 +115,19 @@ CREATE TABLE TicketTable(
 	instructor_id integer,
 	semester integer,
 	year integer,
-	course_id integer,
-	status text -- 'Accepted' , 'Pending' , 'Rejected' 
+	course_id text,
+	status text ,-- 'Accepted' , 'Pending' , 'Rejected' 
+	
+	FOREIGN KEY(student_id) 
+	REFERENCES Students(student_id)
+	ON DELETE CASCADE,
+	
+	FOREIGN KEY(instructor_id) 
+	REFERENCES Instructors(instructor_id)
+	ON DELETE CASCADE,
+	
+	FOREIGN KEY(course_id) 
+	REFERENCES CourseCatalogue(course_id)
+	ON DELETE CASCADE
 );
+
