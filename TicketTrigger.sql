60601537c688bca43cc13f5f544ac135c8905538
Sangram Jagadale <58004845+Sangram3@users.noreply.github.com>
2021-10-28 12:54:37 +0530

    Update TicketTrigger.sql

--- TicketTrigger.sql
+++ TicketTrigger.sql
@@ -1,1 +1,31 @@
+-- When there is update in ticket table trigger invokes this function
 
+CREATE OR REPLACE FUNCTION RegisterTicket()
+RETURNS TRIGGER 
+LANGUAGE plpgsql
+AS
+$$
+
+BEGIN
+	IF NEW.status = 'Accepted' THEN
+		INSERT INTO StudentRegistrationTable VALUES(OLD.student_id ,
+											 OLD,course_id,
+											 OLD.semester,
+											 OLD.year,
+											 OLD.section,
+											 OLD.slot );
+	ELSE
+		DELETE FROM TicketTable WHERE id = OLD.id;
+	
+	END IF;
+	RETURN NEW;
+END;
+$$;
+
+----------------------------------------------------------------------------
+CREATE TRIGGER RecordTicketTable
+	
+	AFTER UPDATE ON TicketTable
+	FOR EACH ROW
+	WHEN (NEW.status ='Accepted' OR NEW.status='Rejected' )
+	EXECUTE function RegisterTicket();
