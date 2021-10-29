3cc67f70042c934578c5ec9cda4e163136ef6248
Harshwardhan Kumar <56429764+Harshiitrpr@users.noreply.github.com>
2021-10-30 01:02:45 +0530

    Update CourseGradesTrigger.sql

--- CourseGradesTrigger.sql
+++ CourseGradesTrigger.sql
@@ -1,2 +1,24 @@
 -- When a certain course is offered then new table is created to store the grades of the students
 -- taking that course
+
+
+
+CREATE OR REPLACE FUNCTION AddEntriesByCsv(
+	IN address text
+)
+
+RETURNS TABLE(
+	course_id text  ,
+	student_id integer  ,
+	year integer  ,
+	semester integer  ,
+	grade integer 
+)
+LANGUAGE plpgsql
+AS
+$$
+	BEGIN
+		RETURN COPY grades FROM address WITH (FORMAT csv);
+	END;
+$$;
+
