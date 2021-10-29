0a2318a1a08778c574e217e81cf040436ab55252
Sangram Jagadale <58004845+Sangram3@users.noreply.github.com>
2021-10-30 01:20:47 +0530

    Update StudentTranscriptDynamic.sql

--- StudentTranscriptDynamic.sql
+++ StudentTranscriptDynamic.sql
@@ -1,3 +1,6 @@
+-- Created a trigger which invokes the below function whenever there is new entry in
+-- student table and generates a transcript table dynamically to store the grades of that student
+
 CREATE OR REPLACE FUNCTION CreateStudentTranscrptTable()
 RETURNS TRIGGER
 LANGUAGE plpgsql
