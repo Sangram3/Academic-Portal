245d54ca741d7f919cb8d70b344e2231e2a85f8f
Sangram Jagadale <58004845+Sangram3@users.noreply.github.com>
2021-10-30 01:37:42 +0530

    Create CourseGradesDynamic.sql

---
+++ CourseGradesDynamic.sql
@@ -0,0 +1,26 @@
+-- when there is entry in CourseOffering table a table is created dynamically to store the grades of that offering
+
+CREATE OR REPLACE FUNCTION CreateCourseGradesTable()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+DECLARE 
+S varchar;
+cond integer;
+
+BEGIN
+S = CONCAT('CREATE TABLE IF NOT EXISTS CourseGrades_',
+		   NEW.course_id,
+		   'Year',NEW.year ,
+		   'Semester',NEW.semester,
+		   'Section',NEW.section,
+		   '(student_id integer PRIMARY KEY , grade integer )');	   
+EXECUTE S;
+RETURN NULL;
+END;
+$$;
+
+CREATE TRIGGER CreateCourseGrades
+	AFTER INSERT ON CourseOfferings
+	FOR EACH ROW 
+	EXECUTE PROCEDURE CreateCourseGradesTable();
