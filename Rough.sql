0fdb13542c57356091d79727023bcada659aa786
Sangram Jagadale <58004845+Sangram3@users.noreply.github.com>
2021-11-01 20:47:54 +0530

    Create Rough.sql

---
+++ Rough.sql
@@ -0,0 +1,18 @@
+  -- create role  boys with user sangram,harsh,john;
+-- grant boys to sangram;
+-- GRANT SELECT 
+-- ON gg 
+-- TO boys;
+
+
+CREATE ROLE Students;
+CREATE ROLE Faculty;
+CREATE ROLE BatchAdvisor;
+CREATE ROLE DeanFA;
+
+
+--Register Function
+GRANT ALL ON FUNCTION Register TO Students;
+GRANT ALL ON FUNCTION CalculateCGPA TO Faculty, DeanFA;
+
+
