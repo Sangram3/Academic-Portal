ea0da1f4e58d27e64af85ada6be0121114a4dbb4
Sangram Jagadale <58004845+Sangram3@users.noreply.github.com>
2021-11-01 21:08:00 +0530

    Create CreateUsers.sql

---
+++ CreateUsers.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION CreateStudent( 
+	IN name_ text,
+	IN password_ text
+)
+RETURNS void
+LANGUAGE plpgsql
+AS
+$$
+DECLARE S text;
+BEGIN
+	S = CONCAT('CREATE USER ',
+		   name_, ' WITH PASSWORD ' , '''  password_ ''' );
+	EXECUTE S;
+	S = CONCAT('GRANT Students TO ',name_);
+	EXECUTE S;  
+END;
+$$
