bec8d70f3789e66bbac224d0fec1c38572d35862
Sangram Jagadale <58004845+Sangram3@users.noreply.github.com>
2021-11-02 20:44:21 +0530

    Update CourseGradesTrigger.sql

--- CourseGradesTrigger.sql
+++ CourseGradesTrigger.sql
@@ -1,25 +1,40 @@
 -- When a certain course is offered then new table is created to store the grades of the students
 -- taking that course
 
-
-
-CREATE OR REPLACE FUNCTION AddEntriesByCsv(
-	IN address text
+-- Course Grade table
+CREATE OR REPLACE FUNCTION AddEntriesByCSVCourseGrade(
+	IN address text,
+	IN course_id_ text,
+	IN year_ integer,
+	IN semester_ integer,
+	IN section_ integer
 )
-
-RETURNS TABLE(
-	course_id text  ,
-	student_id integer  ,
-	year integer  ,
-	semester integer  ,
-	grade integer 
-)
+RETURNS void
 LANGUAGE plpgsql
 SECURITY definer
 AS
 $$
+DECLARE S varchar;
 	BEGIN
-		RETURN COPY grades FROM address WITH (FORMAT csv);
+		EXECUTE format('COPY CourseGrades%sYear%sSemester%sSection%s FROM ''%s'' WITH(FORMAT CSV);',
+					   course_id_ , year_ , semester_,section_,address);
 	END;
 $$;
 
+
+
+-- Grades table	
+CREATE OR REPLACE FUNCTION AddEntriesByCSVGrades(
+	IN address text, 
+)
+RETURNS void
+LANGUAGE plpgsql
+SECURITY definer
+AS
+$$
+DECLARE S varchar;
+	BEGIN
+		S = CONCAT( 'COPY grades from ''',address,''' WITH (FORMAT csv)' );
+		EXECUTE S;
+	END;
+$$;
