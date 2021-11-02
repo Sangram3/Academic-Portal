f6f86f1ae4ac095776c898561019ec58be9a308d
Sangram Jagadale <58004845+Sangram3@users.noreply.github.com>
2021-11-03 00:44:18 +0530

    Update permissions.sql

--- permissions.sql
+++ permissions.sql
@@ -10,16 +10,14 @@ CREATE ROLE Faculty;
 CREATE ROLE BatchAdvisor;
 CREATE ROLE DeanFA;
 
-
---Register Function
 GRANT ALL ON FUNCTION Register TO Students;
 GRANT ALL ON FUNCTION CalculateCGPA TO Faculty, DeanFA;
-GRANT ALL ON FUNCTION CheckGrades TO Students, Faculty, BatchAdvisor, DeanFA;
-GRANT ALL ON FUNCTION CreateCourseGradesTable TO Students, Faculty, BatchAdvisor, DeanFA;
-GRANT ALL ON FUNCTION AddEntriesByCsv TO Students, Faculty, BatchAdvisor, DeanFA;
-GRANT ALL ON FUNCTION CreateStudent TO Students, Faculty, BatchAdvisor, DeanFA;
-GRANT ALL ON FUNCTION CreateFaculty TO Students, Faculty, BatchAdvisor, DeanFA;
-GRANT ALL ON FUNCTION CreateDeanFA TO Students, Faculty, BatchAdvisor, DeanFA;
-GRANT ALL ON FUNCTION CreateStudentTranscrptTable TO Students, Faculty, BatchAdvisor, DeanFA;
-GRANT ALL ON FUNCTION RegisterTicket TO Students, Faculty, BatchAdvisor, DeanFA;
+GRANT ALL ON FUNCTION AddEntriesByCSVCourseGrade TO Faculty;
+GRANT ALL ON FUNCTION CreateCourseGradesTable TO Faculty;
+GRANT ALL ON FUNCTION StudentTranscriptUpdate TO DeanFA;
+GRANT ALL ON FUNCTION CreateStudent TO  DeanFA;
+GRANT ALL ON FUNCTION CreateFaculty TO DeanFA;
+GRANT ALL ON FUNCTION CreateDeanFA TO DeanFA;
+GRANT ALL ON FUNCTION CreateStudentTranscriptTable TO Students, Faculty, BatchAdvisor, DeanFA;
+GRANT ALL ON FUNCTION RegisterTicket TO  DeanFA;
 
